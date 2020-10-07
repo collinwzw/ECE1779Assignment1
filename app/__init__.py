9c752d14a30398ef4f3f3a08bf17f9985f7c27a4
callenfu <56216559+callenfu@users.noreply.github.com>
2020-10-07 22:46:49 +0800

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,4 @@ from flask import Flask # creates the application object as an instance of class
 app = Flask(__name__) #The __name__ variable passed to the Flask class is a Python predefined variable, which is set to the name of the module in which it is used.
 
 
-
-
 from app import main

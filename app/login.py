41e90db902c267e09528a5469cc8afa79b2719d3
callenfu <56216559+callenfu@users.noreply.github.com>
2020-10-19 03:03:22 +0800

    fix email loading delay

--- app/login.py
+++ app/login.py
@@ -1,6 +1,7 @@
 from app import app, mail
 import random
 import string
+import sys
 from flask import render_template, g, request, session, redirect, url_for, flash, current_app
 from app.main import get_db
 from flask_mail import Message
@@ -10,7 +11,6 @@ from app import bootstrap
 
 
 def send_email(subject, sender, recipients, text_body):
-    print(current_app)
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     mail.send(msg)
@@ -113,8 +113,13 @@ def reset_password():
             query = "update accounts set password_hash= %s WHERE email= %s"
             cursor.execute(query, (new_password_hash, useremail))
             cursor.execute("commit")
-            send_password_reset_email(useremail,new_password)
-            flash('Your new password has been sent to your mailbox')
+            try:
+                send_password_reset_email(useremail,new_password)
+                flash('Your new password has been sent to your mailbox')
+            except:
+                e = sys.exc_info()
+                return render_template('resetpassword.html', form=form)
+                flash('Your new password has been sent to your mailbox')
             return redirect(url_for('login'))
         else:
             flash('This email address is not registered')
